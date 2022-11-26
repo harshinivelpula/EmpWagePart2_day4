8149160e7be4b4fe74ebf169bb31c2b8ce96c925
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-26 16:13:42 +0530

    ability to get total wage

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EmpWageFinalSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("total wage for DMart company:" + empWageBuilder.getTotalWage("DMart"));
+        }
+    }
+}
